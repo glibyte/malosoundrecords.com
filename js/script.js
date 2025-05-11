116105ce3481d13aea2b3d09130f5cf47c2bfcdb
Pacsoda <ruizpac98@gmail.com>
2025-05-11 03:31:02 -0600

    v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 const toggle = document.getElementById('menu-toggle');
 const mobileNav = document.getElementById('mobile-nav');
+const header = document.querySelector('header');
 
 toggle.addEventListener('click', () => {
   mobileNav.classList.toggle('show');
+  header.classList.toggle('menu-open');
+});
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
